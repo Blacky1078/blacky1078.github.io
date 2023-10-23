e54ea171134adf5244a4cb50cecc2066e9a053bb
Shivansh Goel <92398906+Blacky1078@users.noreply.github.com>
2023-10-24 03:39:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,55 +1,54 @@
-let userdetails = [];
+let user_login = [];
+
 const button = () => {
   const crfullname = document.getElementById("CRfullname").value;
   const cruservalue = document.getElementById("CRusername").value;
   const crpassvalue = document.getElementById("CRpassword").value;
 
-  userdetails.push({
-    FullName: crfullname,
-    Username: cruservalue,
-    Password: crpassvalue,
-  });
+  const existing_user = user_login.find(
+    (user) => user.Username === cruservalue
+  );
+  console.log();
+  if (existing_user) {
+    alert("Existing User");
+  } else {
+    user_login.push({
+      Key: user_login.length,
+      FullName: crfullname,
+      Username: cruservalue,
+      Password: crpassvalue,
+    });
 
-  localStorage.setItem("USERS", JSON.stringify(userdetails));
+    localStorage.setItem("user_login", JSON.stringify(user_login));
+    document.getElementById("Create_modal").style.display = "block";
+    document.getElementById("CRFN").append(" " + crfullname + ",");
+    document.getElementById("u").append(" " + cruservalue);
+    document.getElementById("p").append(" " + crpassvalue);
+  }
 };
 
-const Storeddata = JSON.parse(localStorage.getItem("USERS"));
-const len = userdetails.length;
+const Storeddata = JSON.parse(localStorage.getItem("user_login"));
 
-const Passwords = Storeddata.forEach((element) => {});
-// console.log(Usernames)
+let n;
 const button2 = () => {
-  Storeddata.forEach((element) => {
-    if (
-      document.getElementById("Username").value == element.Username &&
-      document.getElementById("Password").value == element.Password
-    ) {
-      const page1 = document.getElementById("loading");
-      page1.style.display = "block";
-
-      const page0 = document.getElementById("Login_page");
-      page0.style.display = "none";
-
-      const loading = document.getElementById("progress");
-      loading.style.animationName = "Loading";
-      loading.style.animationDuration = "4s";
-      loading.style.animationIterationCount = "infinite";
-
-      const button = document.createElement("button");
-      button.setAttribute("class", "btn btn-danger");
-      button.style.fontSize = "20px";
-      button.style.marginLeft = "100px";
-      button.style.marginTop = "100px";
-      const a = document.createElement("a");
-      a.href = "https://blacky1078.github.io/Users/user.html";
-      a.append("Click Here");
-
-      button.append(a);
-      page1.append(button);
-    }
-  });
+  const login_user = Storeddata.find(
+    (user) =>
+      user.Username === document.getElementById("Username").value &&
+      user.Password === document.getElementById("Password").value
+  );
+  console.log(login_user);
+  if (login_user) {
+    document.getElementById("Login_modal").style.display = "block";
+  } else {
+    alert("Invalid Credentials");
+  }
 };
-
+const close1 = () => {
+  document.getElementById("Login_modal").style.display = "none";
+};
+const close2 = () => {
+  document.getElementById("Create_modal").style.display = "none";
+};
 const theme = (event) => {
   const value = event.target.value;
   document.querySelector("html").setAttribute("data-bs-theme", value);
@@ -81,12 +80,12 @@ const button_5 = () => {
 const local2 = JSON.parse(localStorage.getItem("TODOing"));
 local1.forEach((element) => {
   const tododiv = document.getElementById("todo_div");
-  const item = element;
+  let item = element;
   tododiv.innerHTML += `<li>${item}</li>`;
 });
 
 local2.forEach((element) => {
   const todoingdiv = document.getElementById("todoing_div");
-  const item = element;
+  let item = element;
   todoingdiv.innerHTML += `<li>${item}</li>`;
 });
